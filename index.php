bd5f3f8619037f58bf7890fd7e7496a9f732f54b
AnvayD15 <anvayd01@gmail.com>
2021-11-28 13:03:26 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title></title>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" type="text/css" href="style.css">
+     <script src="app.js"></script>
+    
+
+     </head>
+     <body>
+
+     <?php include_once('header.php') ?>
+     
+      <div class="homepage">
+        <img class="homepage" src="homepage image.jpg">
+        <div class="centered">Driving made easy with 
+           our professinally skilled drivers</div>
+            </div>
+            <div class="content">
+            <div class="head">
+              <p>Our Services</p>
+            </div>
+
+            
+            <div class="overview">
+             <div class="heading"> <p>An overview of 
+                what we do</p>
+              </div>
+              <div class="todo">
+                <p>
+                  Lorem ipsum dolor sit amet, 
+                  consectetur adipiscing elit. 
+                  Suspendisse placerat risus lacus, 
+                  feugiat volutpat ante tempor ut. 
+                  Sed pellentesque gravida
+                </p>
+              </div>
+         
+                
+
+            </div>
+            </div>
+            
+            
+            
+
+              <div class="vision">
+                
+                <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="card" style="width:75%">
+                                        <div class="card-title">
+                                            Experiened Instructors
+                                        </div>
+                                        <div class="card-body" >
+                                            
+                                        </div>
+                                        <div class="card-text">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima eum architecto eaque. Dignissimos eaque voluptate quam veniam, soluta cupiditate quo quibusdam error unde a accusantium porro numquam est iste consequatur?
+                                        </div>
+            
+                                    </div>
+                                    
+                                    
+                                </div>
+                                <div class="row justify-content-center">
+                                    <div class="card" style="width:75%">
+                                        <div class="card-title">
+                                          Women Oriented Services
+                                        </div>
+                                        <div class="card-body" >
+                                            
+                                        </div>
+                                        <div class="card-text">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima eum architecto eaque. Dignissimos eaque voluptate quam veniam, soluta cupiditate quo quibusdam error unde a accusantium porro numquam est iste consequatur?
+                                        </div>
+            
+                                    </div>
+                                </div>
+                                <div class="row justify-content-center">
+                                    <div class="card" style="width:75%">
+                                        <div class="card-title">
+                                          Driving Success
+                                        </div>
+                                        <div class="card-body" >
+                                            
+                                        </div>
+                                        <div class="card-text">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima eum architecto eaque. Dignissimos eaque voluptate quam veniam, soluta cupiditate quo quibusdam error unde a accusantium porro numquam est iste consequatur?
+                                        </div>
+            
+                                    </div>
+                                </div>
+                </div>
+            </div>
+
+            
