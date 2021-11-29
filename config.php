cb4213972a66f03e8da282742bb43515336745e3
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2021-11-29 10:31:21 +0530

    delete

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$SETTINGS["mysql_user"]='user';
-$SETTINGS["mysql_pass"]='pass';
-$SETTINGS["hostname"]='localhost';
-$SETTINGS["mysql_database"]='demo';
-$SETTINGS["data_table"]='registrations';
-$SETTINGS["paypal_address"]='email@domain.com';
-?>
